a9559f9bcd1f47c0098181cacd51f1636afea73b
Anjudimpy <anjukr55555@gmail.com>
2025-06-17 20:00:32 +0530

    Redux middelware

---
+++ Redux/src(redux middleware)/redux/middlewares/loggerMiddleware.js
@@ -0,0 +1,12 @@
+// create and export middleware function here
+export const loggerMiddleware = (store)=>{
+    return function(next){
+        return function(action){
+            console.log("[LOG]: "+action.type+ " "+ new Date().toString());
+            next(action);
+            console.log(store.getState());
+        }
+    }
+}
+
+
